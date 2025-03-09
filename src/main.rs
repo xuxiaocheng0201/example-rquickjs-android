f6d8be95845b8454ff410cde9ca9e5e372970173
XuXiaoCheng <2762267080@qq.com>
2025-03-09 19:38:27 +0800

    Directly use env to build on android

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> rquickjs::Result<()> {
+    let runtime = rquickjs::Runtime::new()?;
+    let context = rquickjs::Context::full(&runtime)?;
+    let string = context.with(|ctx| ctx.eval::<String, _>("'Hello, World!'"))?;
+    println!("{string}");
+    Ok(())
 }
